be95ed3947264bb67a159ceaa13599a3e6ce7d63
mason tan <masonev@gmail.com>
2020-02-21 15:19:28 +0100

    deploy fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,83 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cocktail.destroy_all
+Ingredient.delete_all
+# Review.destroy_all
+puts "cleaning done"
+
+cocktails_attributes = [
+  {
+    name: "Light rum"
+  },
+  {
+    name: "Applejack"
+  },
+  {
+    name: "Gin"
+  },
+  {
+    name: "Dark rum"
+  },
+  {
+    name: "Sweet Vermouth"
+  },
+  {
+    name: "Strawberry schnapps"
+  },
+  {
+    name: "Scotch"
+  },
+  {
+    name: "Apricot brandy"
+  },
+  {
+    name: "Triple sec"
+  },
+  {
+    name: "Southern Comfort"
+  }
+]
+
+cocktails = Cocktail.create!(cocktails_attributes)
+p cocktails
+
+puts 'Cleaning ingredients table'
+
+
+ingredients_attributes =[
+  {
+    name: "gin"
+  },
+  {
+    name: "rum"
+  },
+  {
+    name: "bourbon"
+  },
+  {
+    name: "brandy"
+  },
+  {
+    name: "whiskey"
+  },
+  {
+    name: "tequila"
+  },
+  {
+    name: "scotch"
+  },
+  {
+    name: "vodka"
+  },
+  {
+    name: "cognac "
+  },
+  {
+    name: "tea"
+  }
+]
+ingredients = Ingredient.create!(ingredients_attributes)
+p ingredients
+
+puts "seed done"
