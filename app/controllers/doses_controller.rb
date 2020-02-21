adc527a55dc9b0fa67a9caa0a2283dc0d9acb7b0
mason tan <masonev@gmail.com>
2020-02-21 15:30:14 +0100

    fixed

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -15,11 +15,11 @@ class DosesController < ApplicationController
   end
   
   def destroy
-    raise
-    # @dose = Dose.find(params[:id])
-    # @dose.destroy
-    # redirect_to cocktail_path(@dose.cocktail)
-    # raise
+  
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  
   end
   
   private
